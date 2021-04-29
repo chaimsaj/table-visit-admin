309b2276d1128b044f3f7de59bdb70b51ce05a14
Oleg Poustovalov <olegp@aolideas.com>
2021-04-29 15:06:18 -0300

    -

--- webapp/resources/views/layouts/master.blade.php
+++ webapp/resources/views/layouts/master.blade.php
@@ -3,10 +3,10 @@
 
 <head>
     <meta charset="utf-8" />
-    <title> @yield('title') | Skote - Admin & Dashboard Template</title>
+    <title> @yield('title') | Table Visit</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
+    <meta content="Table Visit" name="description" />
+    <meta content="Table Visit" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon.ico') }}">
     @include('layouts.head-css')
