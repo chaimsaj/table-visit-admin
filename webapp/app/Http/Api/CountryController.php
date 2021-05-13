08834662c08f560dbb162ca5d8758ec4fd51a3e4
Oleg Poustovalov <olegp@aolideas.com>
2021-05-13 19:19:35 -0300

    -

---
+++ webapp/app/Http/Api/CountryController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Http\Api;
+
+
+use Illuminate\Routing\Controller as BaseController;
+
+class CountryController extends BaseController
+{
+
+}
---
+++ webapp/app/Http/Api/MainController.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Http\Api;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class MainController extends BaseController
+{
+
+}
