31fbe8c9fed51e67ebdf8ad434a142b397ca6eb9
Oleg Poustovalov <olegp@aolideas.com>
2021-05-25 15:46:22 -0300

    -

--- webapp/resources/views/index.blade.php
+++ webapp/resources/views/index.blade.php
@@ -10,7 +10,7 @@
     @endcomponent
 
     <!-- subscribeModal -->
-    <div class="modal fade" id="subscribeModal" tabindex="-1" aria-labelledby="subscribeModalLabel" aria-hidden="true">
+    {{--<div class="modal fade" id="subscribeModal" tabindex="-1" aria-labelledby="subscribeModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header border-bottom-0">
@@ -47,7 +47,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>--}}
     <!-- end modal -->
 
 @endsection
