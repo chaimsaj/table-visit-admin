43efe662195b55132148844d22966fe40a478f4f
Oleg Poustovalov <olegp@aolideas.com>
2021-05-26 16:07:37 -0300

    -

--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -17,32 +17,26 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'root'])->name('root');
-
-//Update User Details
-Route::post('/update-profile/{id}', [App\Http\Controllers\HomeController::class, 'updateProfile'])->name('updateProfile');
-Route::post('/update-password/{id}', [App\Http\Controllers\HomeController::class, 'updatePassword'])->name('updatePassword');
-
-
-
-//Route::get('{any}', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+Route::get('/index', [App\Http\Controllers\HomeController::class, 'root'])->name('root');
 
 //Language Translation
 Route::get('index/{locale}', [App\Http\Controllers\HomeController::class, 'lang']);
 
+//Update User Details
+//Route::post('/update-profile/{id}', [App\Http\Controllers\HomeController::class, 'updateProfile'])->name('updateProfile');
+//Route::post('/update-password/{id}', [App\Http\Controllers\HomeController::class, 'updatePassword'])->name('updatePassword');
+//Route::get('/users', 'App\Http\Controllers\UserController@index');
+//Route::post('/users-create', [App\Http\Controllers\UserController::class, 'store'])->name('store');
+//Route::get('/users-create', 'App\Http\Controllers\UserController@create');
+//Route::post('/users-edit', [App\Http\Controllers\UserController::class, 'update'])->name('update');
+//Route::get('/users-edit/{id}', 'App\Http\Controllers\UserController@edit');
 
+//Users
+Route::get('/users', [App\Http\Controllers\UsersController::class, 'index'])->name('users');
+Route::get('/user/{id}', [App\Http\Controllers\UsersController::class, 'detail'])->name('user.detail');
+Route::post('/user/{id}', [App\Http\Controllers\UsersController::class, 'save'])->name('user.save');
 
-
-Route::get('/users', 'App\Http\Controllers\UserController@index');
-
-Route::post('/users-create', [App\Http\Controllers\UserController::class, 'store'])->name('store');
-Route::get('/users-create', 'App\Http\Controllers\UserController@create');
-
-Route::post('/users-edit', [App\Http\Controllers\UserController::class, 'update'])->name('update');
-Route::get('/users-edit/{id}', 'App\Http\Controllers\UserController@edit');
-
-//Route::post('/user-detail', [App\Http\Controllers\UserController::class, 'editUser'])->name('editUser');
-// Route::get('/users', [App\Http\Controllers\UserController::class, 'index']);
-//Route::resource('users','App\Http\Controllers\UserController@index');
-//
-//Route::post('/users', 'App\Http\Controllers\UserController@index');
-
+//Countries
+Route::get('/countries', [App\Http\Controllers\CountriesController::class, 'index'])->name('countries');
+Route::get('/country/{id}', [App\Http\Controllers\CountriesController::class, 'detail'])->name('country.detail');
+Route::post('/country/{id}', [App\Http\Controllers\CountriesController::class, 'save'])->name('country.save');
