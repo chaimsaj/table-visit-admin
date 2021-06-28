40e9e1ca35f797b62805e15eb5f1c7f5532bbd77
Oleg Poustovalov <olegp@aolideas.com>
2021-06-28 16:12:12 -0300

    -

--- webapp/app/Http/Controllers/CitiesController.php
+++ webapp/app/Http/Controllers/CitiesController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 
 class CitiesController extends BasicController
 {
-    private $service;
+    private CityServiceInterface $service;
 
     public function __construct(CityServiceInterface $service)
     {
--- webapp/resources/views/cities/index.blade.php
+++ webapp/resources/views/cities/index.blade.php
@@ -23,9 +23,46 @@
                                         class="mdi mdi-plus me-1"></i> Add new
                                 </a>
                             </div>
-                        </div><!-- end col-->
+                        </div>
                     </div>
                     <hr/>
+                    <table id="datatable"
+                           class="table table-bordered dt-responsive nowrap w-100 align-middle">
+                        <thead class="table-light">
+                        <tr class="text-center">
+                            <th>@lang('translation.Id')</th>
+                            <th>@lang('translation.Name')</th>
+                            <th>@lang('translation.Code')</th>
+                            <th>@lang('translation.DisplayOrder')</th>
+                            <th>@lang('translation.State')</th>
+                            <th>@lang('translation.Country')</th>
+                            <th class="no-sort">@lang('translation.Delete')</th>
+                            <th class="no-sort">@lang('translation.Edit')</th>
+                        </tr>
+                        </thead>
+
+
+                        <tbody>
+                        @foreach ($data as $item)
+                            <tr class="text-center">
+                                <td>{{ $item->id }}</td>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->iso_code }}</td>
+                                <td>{{ $item->display_order }}</td>
+                                <td>{{ $item->state_name }}</td>
+                                <td>{{ $item->country_name }}</td>
+                                <td>
+                                    <a href="{{route("city.delete", $item->id)}}" class="text-danger"><i
+                                            class="mdi mdi-delete font-size-18"></i></a>
+                                </td>
+                                <td>
+                                    <a href="{{route("city.detail", $item->id)}}" class="text-success"><i
+                                            class="mdi mdi-pencil font-size-18"></i></a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
