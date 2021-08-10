dc04a984dd511601ad54617576209d72a4982ee8
Oleg Poustovalov <olegp@aolideas.com>
2021-08-10 12:22:24 -0300

    -

--- webapp/app/Http/Controllers/ServiceRatesController.php
+++ webapp/app/Http/Controllers/ServiceRatesController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Base\AdminController;
-use App\Repositories\ServiceRateRepository;
+use App\Models\ServiceType;
 use App\Repositories\ServiceRateRepositoryInterface;
 use App\Services\LogServiceInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Throwable;
 
 class ServiceRatesController extends AdminController
 {
@@ -22,21 +24,50 @@ class ServiceRatesController extends AdminController
 
     public function index()
     {
-        return view('service-rates/index');
+        $data = $this->serviceRateRepository->actives();
+        return view('service-rates/index', ["data" => $data]);
     }
 
-    public function detail()
+    public function detail($id)
     {
-        return view('service-rates/detail');
+        $data = $this->serviceRateRepository->find($id);
+        return view('service-rates/detail', ["data" => $data]);
     }
 
     public function save(Request $request, $id)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => ['required', 'string', 'max:255'],
+            ]);
+
+            $db = $this->serviceRateRepository->find($id);
+
+            if ($validator->fails() && $db == null) {
+                return view('service-rates/detail', ["data" => $request])->withErrors($validator);
+            } else {
+                if ($db == null)
+                    $db = new ServiceType();
+
+                $db->name = $request->get('name');
+                $db->display_order = intval($request->get('display_order'));
+                $db->published = $request->get('published') == "on";
+                $db->show = $request->get('show') == "on";
+
+                $this->serviceRateRepository->save($db);
+            }
+
+        } catch (Throwable $ex) {
+            $this->logger->save($ex);
+        }
+
+        return redirect("service-rates");
     }
 
     public function delete($id)
     {
-        //
+        $this->serviceRateRepository->deleteLogic($id);
+
+        return redirect("service-rates");
     }
 }
--- webapp/app/Http/Controllers/TableRatesController.php
+++ webapp/app/Http/Controllers/TableRatesController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Base\AdminController;
-use App\Repositories\ServiceRepositoryInterface;
+use App\Models\ServiceType;
 use App\Repositories\TableRateRepositoryInterface;
-use App\Repositories\TableRepositoryInterface;
-use App\Repositories\TableTypeRepositoryInterface;
 use App\Services\LogServiceInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Throwable;
 
 class TableRatesController extends AdminController
 {
@@ -24,21 +24,50 @@ class TableRatesController extends AdminController
 
     public function index()
     {
-        return view('table-rates/index');
+        $data = $this->tableRateRepository->actives();
+        return view('table-rates/index', ["data" => $data]);
     }
 
-    public function detail()
+    public function detail($id)
     {
-        return view('table-rates/detail');
+        $data = $this->tableRateRepository->find($id);
+        return view('table-rates/detail', ["data" => $data]);
     }
 
     public function save(Request $request, $id)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => ['required', 'string', 'max:255'],
+            ]);
+
+            $db = $this->tableRateRepository->find($id);
+
+            if ($validator->fails() && $db == null) {
+                return view('table-rates/detail', ["data" => $request])->withErrors($validator);
+            } else {
+                if ($db == null)
+                    $db = new ServiceType();
+
+                $db->name = $request->get('name');
+                $db->display_order = intval($request->get('display_order'));
+                $db->published = $request->get('published') == "on";
+                $db->show = $request->get('show') == "on";
+
+                $this->tableRateRepository->save($db);
+            }
+
+        } catch (Throwable $ex) {
+            $this->logger->save($ex);
+        }
+
+        return redirect("table-rates");
     }
 
     public function delete($id)
     {
-        //
+        $this->serviceRateRepository->deleteLogic($id);
+
+        return redirect("table-rates");
     }
 }
