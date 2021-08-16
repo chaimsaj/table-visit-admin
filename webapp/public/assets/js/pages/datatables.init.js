108500ca781935f7abd72d8265dd4da34574cd38
Oleg Poustovalov <olegp@aolideas.com>
2021-08-16 11:28:03 -0300

    Update datatables.init.js

--- webapp/public/assets/js/pages/datatables.init.js
+++ webapp/public/assets/js/pages/datatables.init.js
@@ -3,19 +3,13 @@ var __webpack_exports__ = {};
 /*!***********************************************!*\
   !*** ./resources/js/pages/datatables.init.js ***!
   \***********************************************/
-/*
-Template Name: Skote - Admin & Dashboard Template
-Author: Themesbrand
-Website: https://themesbrand.com/
-Contact: themesbrand@gmail.com
-File: Datatables Js File
-*/
 $(document).ready(function () {
   $('#datatable').DataTable({
     columnDefs: [{
       targets: 'no-sort',
       orderable: false
-    }]
+    }],
+    order: [[1, "asc"]]
   }); //Buttons examples
 
   var table = $('#datatable-buttons').DataTable({
