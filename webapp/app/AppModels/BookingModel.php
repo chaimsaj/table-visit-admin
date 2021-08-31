cf46ba8c0a53b9ba8a98e1af79e068d4eacb7eea
Oleg Poustovalov <olegp@aolideas.com>
2021-08-31 15:06:11 -0300

    -

---
+++ webapp/app/AppModels/BookingModel.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\AppModels;
+
+
+class BookingModel
+{
+    public string $comment;
+    public int $place_id;
+    public int $table_id;
+}
--- webapp/app/Http/Api/PaymentsController.php
+++ webapp/app/Http/Api/PaymentsController.php
@@ -6,38 +6,23 @@ namespace App\Http\Api;
 use App\AppModels\ApiModel;
 use App\Http\Api\Base\ApiController;
 use App\Repositories\BookingRepositoryInterface;
+use App\Repositories\PaymentRepositoryInterface;
 use App\Repositories\TableRepositoryInterface;
 use App\Services\LogServiceInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Throwable;
 
-class BookingsController extends ApiController
+class PaymentsController extends ApiController
 {
-    private BookingRepositoryInterface $bookingRepository;
+    private PaymentRepositoryInterface $paymentRepository;
 
-    public function __construct(TableRepositoryInterface $bookingRepository,
-                                LogServiceInterface      $logger)
+    public function __construct(PaymentRepositoryInterface $paymentRepository,
+                                LogServiceInterface        $logger)
     {
         parent::__construct($logger);
 
-        $this->bookingRepository = $bookingRepository;
-    }
-
-    public function book(): JsonResponse
-    {
-        $response = new ApiModel();
-        $response->setSuccess();
-
-        try {
-            $user = Auth::user();
-
-
-        } catch (Throwable $ex) {
-            $this->logger->save($ex);
-        }
-
-        return response()->json($response);
+        $this->paymentRepository = $paymentRepository;
     }
 
     public function list(): JsonResponse
