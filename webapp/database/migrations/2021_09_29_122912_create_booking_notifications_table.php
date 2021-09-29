b4c39ad7e9499763327b2181db3d26a88403cce4
Oleg Poustovalov <olegp@aolideas.com>
2021-09-29 09:29:35 -0300

    -

---
+++ webapp/database/migrations/2021_09_29_122912_create_booking_notifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking_notifications', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
+            $table->boolean('published')->default(true);
+            $table->boolean('deleted')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking_notifications');
+    }
+}
