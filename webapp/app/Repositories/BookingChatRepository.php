ee15f5869f87c35cb7309d47b69d9391cfc8ca96
Oleg Poustovalov <olegp@aolideas.com>
2021-11-03 10:59:57 -0300

    Update BookingChatRepository.php

--- webapp/app/Repositories/BookingChatRepository.php
+++ webapp/app/Repositories/BookingChatRepository.php
@@ -3,6 +3,7 @@
 
 namespace App\Repositories;
 
+use App\Core\BookingChatStatusEnum;
 use App\Models\BookingChat;
 use App\Repositories\Base\BaseRepository;
 use Illuminate\Database\Eloquent\Model;
@@ -17,19 +18,25 @@ class BookingChatRepository extends BaseRepository implements BookingChatReposit
 
     public function loadBy(int $booking_id, int $chat_type): ?Model
     {
+        $status = BookingChatStatusEnum::Opened;
+
         return $this->model->where('deleted', '=', 0)
             ->where('published', '=', 1)
             ->where('booking_id', '=', $booking_id)
             ->where('chat_type', '=', $chat_type)
+            ->where('chat_status', '=', $status)
             ->orderBy('id', 'desc')
             ->first();
     }
 
     public function loadByUser(int $from_user_id): Collection
     {
+        $status = BookingChatStatusEnum::Opened;
+
         return $this->model->where('deleted', '=', 0)
             ->where('published', '=', 1)
             ->where('from_user_id', '=', $from_user_id)
+            ->where('chat_status', '=', $status)
             ->orderBy('id', 'desc')
             ->get();
     }
